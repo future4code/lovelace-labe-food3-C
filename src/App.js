0ff2e059fba7c25aa9c7f9cdfedf7f426685c3a9
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-09-06 10:07:12 -0400

    Estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Router from "./routes/Router";
+import {CssBaseline} from "@material-ui/core";
+import GlobalStateContext from "./global/GlobalStateContext";
+import {ThemeProvider} from "styled-components";
+import theme from "./constants/theme";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <GlobalStateContext>
+          <CssBaseline/>
+          <Router/>
+        </GlobalStateContext>
+      </ThemeProvider>
   );
 }
 
