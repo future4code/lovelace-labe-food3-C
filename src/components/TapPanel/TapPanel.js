edcd25641c2a2bae1c027807a2756f46b7285ec9
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-06 16:56:41 -0300

    TapPAnel shows cards

--- src/components/TapPanel/TapPanel.js
+++ src/components/TapPanel/TapPanel.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
@@ -41,12 +40,14 @@ function a11yProps(index) {
   };
 }
 
-export default function ScrollableTabsButtonAuto() {
+export default function TapPanel({restaurants}) {
   const [value, setValue] = React.useState(0);
+  const typeRestaurants = Object.keys(restaurants)
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
 
   return (
     <StyledTapPanel>
@@ -60,36 +61,21 @@ export default function ScrollableTabsButtonAuto() {
           scrollButtons="auto"
           aria-label="scrollable auto tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
+          {typeRestaurants && typeRestaurants.map((typeRestaurantName, index) => (
+            <Tab label={typeRestaurantName} {...a11yProps(index)} key={index}/>
+          ))}
         </StyledTabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One111
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+      {typeRestaurants.map((typeRestaurant, index) => (
+        <TabPanel value={value} index={index} key={index}>
+          {restaurants && restaurants[ typeRestaurants[index] ].map(restaurant => (
+            // CARD
+            <div key={restaurant.name}>
+              <p>{restaurant.name} - {typeRestaurant}</p>
+            </div>
+          ))}
+        </TabPanel>
+      ))}
     </StyledTapPanel>
   );
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -11,11 +11,65 @@ const HomePage = () => {
     useProtectedPage()
     const history = useHistory()
 
+    const restaurants = {
+      tipo1: [
+          {name: 'restaurante1', category: 'tipo1'},
+          {name: 'restaurante2', category: 'tipo1'},
+          {name: 'restaurante3', category: 'tipo1'},
+        ],
+      tipo2: [
+        {name: 'restaurante4', category: 'tipo2'},
+        {name: 'restaurante5', category: 'tipo2'},
+        {name: 'restaurante6', category: 'tipo2'},
+        {name: 'restaurante7', category: 'tipo2'},
+      ],
+      tipo3: [
+        {name: 'restaurante8', category: 'tipo3'},
+        {name: 'restaurante9', category: 'tipo3'},
+        {name: 'restaurante10', category: 'tipo3'},
+        {name: 'restaurante11', category: 'tipo3'},
+      ],
+      tipo4: [
+        {name: 'restaurante1', category: 'tipo1'},
+        {name: 'restaurante2', category: 'tipo1'},
+        {name: 'restaurante3', category: 'tipo1'},
+      ],
+      tipo5: [
+        {name: 'restaurante4', category: 'tipo2'},
+        {name: 'restaurante5', category: 'tipo2'},
+        {name: 'restaurante6', category: 'tipo2'},
+        {name: 'restaurante7', category: 'tipo2'},
+      ],
+      tipo6: [
+        {name: 'restaurante8', category: 'tipo3'},
+        {name: 'restaurante9', category: 'tipo3'},
+        {name: 'restaurante10', category: 'tipo3'},
+        {name: 'restaurante11', category: 'tipo3'},
+      ],
+      tipo7: [
+        {name: 'restaurante1', category: 'tipo1'},
+        {name: 'restaurante2', category: 'tipo1'},
+        {name: 'restaurante3', category: 'tipo1'},
+      ],
+      tipo8: [
+        {name: 'restaurante4', category: 'tipo2'},
+        {name: 'restaurante5', category: 'tipo2'},
+        {name: 'restaurante6', category: 'tipo2'},
+        {name: 'restaurante7', category: 'tipo2'},
+      ],
+      tipo9: [
+        {name: 'restaurante8', category: 'tipo3'},
+        {name: 'restaurante9', category: 'tipo3'},
+        {name: 'restaurante10', category: 'tipo3'},
+        {name: 'restaurante11', category: 'tipo3'},
+      ],
+    }
+
 
     return (
         <StyledHomePage>
         <SearchField />
-        <TapPanel />
+        <TapPanel restaurants={restaurants}/>
       </StyledHomePage>
     )
 }
