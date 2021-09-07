fa17d880a0bbe3fffd1c1718cfc276ac849ceb2b
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-07 11:06:10 -0300

    align search

--- src/components/SearchField/styled.js
+++ src/components/SearchField/styled.js
@@ -5,9 +5,11 @@ export const StyledForm = styled(FormControl)`
     width: 100vw;
     padding: 1rem 0.503rem 1rem 1.063rem;
     border-radius: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
     &>div {
-        /* border: solid 1px #b8b8b8; */
         height: 3.5rem;
         width: 20.5rem;
     }
