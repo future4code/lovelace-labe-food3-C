b8e03771b2645afb7e3ff71120285e867cbe7650
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-08 14:48:59 -0300

    Condicional do titulo da Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,59 @@
 import React from 'react'
 import { StyledHeader, StyledMargin } from "./styled"
 import imgBackArrow from "../../assets/images/back.svg"
+import { useHistory } from 'react-router'
+import { useParams } from 'react-router-dom'
 
 const Header = () => {
+    const history = useHistory()
+    const params = useParams()
 
+    let headerText = ""
+    
+    switch (history.location.pathname) {
+        case '/home':
+            headerText = "iFuture"
+            break
+
+        case '/signup':
+            headerText = ""
+            break 
+
+        case `/restaurant/${params.id}`:
+            headerText = "Restaurante"
+            break
+
+        case '/profile':
+            headerText = "Meu Perfil"
+            break
+
+        case '/profile/edit/address':
+            headerText = "Meu Endereço"
+            break    
+
+        case '/profile/edit/user':
+            headerText = "Editar"
+            break
+
+        case '/cart':
+            headerText = "Meu carrinho"
+            break    
+
+        default:
+            headerText = "iFuture"
+            break
+        
+    }
 
     return (
         <>
             <StyledMargin />
             <StyledHeader>
-                {/* Onclick tem que ser um GoBack */}
+                
                 <button onClick={() => null}>
                     <img src={imgBackArrow} alt='botão para voltar'/>
                 </button>
-                <p>iFuture</p>
+                <p>{headerText}</p>
             
             </StyledHeader>
         </>
