cd67ffb0ddd9b13b94c9d2319397ae931b614b3a
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-09 16:11:58 -0300

    Bug Fix - Headers

--- src/hooks/useRequestData.js
+++ src/hooks/useRequestData.js
@@ -6,9 +6,16 @@ const useRequestData = (endpoint, initialState) => {
     const [data, setData] = useState(initialState)
 
     useEffect(() => {
-        const token = localStorage.getItem("token")
+        
 
-        axios.get(`${BASE_URL}${endpoint}`, HEADERS)
+        const token = localStorage.getItem("token")
+        const headers = {
+        headers: {
+            auth: token
+        }
+        }
+
+        axios.get(`${BASE_URL}${endpoint}`, headers)
             .then((response) => {
                 setData(response.data)
                 // console.log(response.data)
@@ -24,3 +31,33 @@ const useRequestData = (endpoint, initialState) => {
 }
 
 export default useRequestData
+
+
+
+// >>>>>>>>>>> Fiz outro useRequestData !!! ABAIXO ...
+
+export const useRequestDataX = (url, initialState) => {
+    const [data, setData] = useState(initialState)
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        const headers = {
+            headers: {
+                auth: token
+            }
+    }
+
+        axios.get(url, headers)
+            .then((response) => {
+                setData(response.data)
+                // console.log("RESPONSE", response.data)
+            })
+            .catch((error) => {
+                // console.log("ERRO GET FULL ADD", error)
+                alert('Ocorreu um erro, tente novamente')
+            })
+
+    }, [url])
+
+    return [data, setData]
+}
--- src/services/getRestaurants.js
+++ src/services/getRestaurants.js
@@ -3,7 +3,13 @@ import {BASE_URL, HEADERS} from "../constants/urls";
 import categorizeRestaurants from "../tools/categorizeRestaurants"
 
 export const getRestaurants = (saveData, setIsLoading) => {
-    axios.get(`${BASE_URL}/fourFoodA/restaurants`, HEADERS)
+    const token = localStorage.getItem("token")
+    const headers = {
+        headers: {
+            auth: token
+        }
+    }
+    axios.get(`${BASE_URL}/fourFoodA/restaurants`, headers)
         .then((response) => {
             saveData(categorizeRestaurants(response.data.restaurants))
             setIsLoading(false)
