899f051f5161cb21e6bfbf73d889ede86d974f0e
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-09 11:11:20 -0300

    add isLoading

--- src/services/getRestaurants.js
+++ src/services/getRestaurants.js
@@ -2,14 +2,13 @@ import axios from "axios";
 import {BASE_URL, HEADERS} from "../constants/urls";
 import categorizeRestaurants from "../tools/categorizeRestaurants"
 
-export const getRestaurants = (saveData) => {
+export const getRestaurants = (saveData, setIsLoading) => {
     axios.get(`${BASE_URL}/fourFoodA/restaurants`, HEADERS)
         .then((response) => {
             saveData(categorizeRestaurants(response.data.restaurants))
-
+            setIsLoading(false)
         })
         .catch((err) => {
-
             console.log(err)
         })
 }
