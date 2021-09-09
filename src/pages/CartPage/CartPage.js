423adfb7a20849bacb45aac03a69b20c6961f510
root <root@acer>
2021-09-09 14:51:42 -0400

    separado css

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -6,156 +6,9 @@ import {Typography, Radio, RadioGroup, FormControlLabel, Button} from "@material
 import Footer from "../../components/Footer/Footer";
 import CardProduct from "../../components/CardProduct/CardProduct";
 import GlobalContext from "../../global/GlobalContext";
+import {CardContainer, Frete, MainContainer, Payments, RestaurantAddress, SubTotal, UserAddress} from "./styled";
 
 
-const MainContainer = styled.div`
-  /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
-`
-
-const UserAddress = styled.div`
-  /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    background-color: #EEE;
-    padding: 20px;
-    //border: 1px solid #000;
-
-  }
-`
-
-const RestaurantAddress = styled.div`
-  /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    padding: 10px 20px;
-    //border: 1px solid #000;
-
-  }
-`
-
-const CardContainer = styled.div`
-  /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    padding: 10px 20px;
-    // border: 1px solid #000;
-
-  }
-`
-
-const Frete = styled.div`
-    /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    text-align: right;
-    padding: 0px 20px;
-    // border: 1px solid #000;
-
-  }
-`
-
-const SubTotal = styled.div`
-    /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 0px 20px;
-    // border: 1px solid #000;
-
-  }
-`
-
-const Payments = styled.div`
-     /* 580px */
-  @media (max-width: 36em) {
-
-  }
-
-  /* 520px */
-  @media (max-width: 32em) {
-
-  }
-
-  /* 480px */
-  @media (max-width: 30em) {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-itens: flex-start;
-    padding: 0px 20px;
-    // border: 1px solid #000;
-
-  }
-`
 
 const CartPage = () => {
     useProtectedPage()
@@ -168,8 +21,7 @@ const CartPage = () => {
         const exeploNomeRestaurante = 'Massas Suaves'
         setters.setWaitingDelivery(exemploTempoMin, exemploSubtotal, exeploNomeRestaurante)
     }
-
-
+    
     return (
         <MainContainer>
 
