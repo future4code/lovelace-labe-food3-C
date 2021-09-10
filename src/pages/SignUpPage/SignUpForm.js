3e2e9ee423ff4e2ef6a751f71ea353d6d9d044d5
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-10 16:27:14 -0300

    Hide/Show Password na tela de Login e SignUp

--- src/pages/SignUpPage/SignUpForm.js
+++ src/pages/SignUpPage/SignUpForm.js
@@ -1,18 +1,30 @@
 import React, {useContext, useState} from 'react'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
 import { InputsContainer, SignUpFormContainer } from './styled'
 import { useHistory } from 'react-router-dom'
 import useForm from '../../hooks/useForm'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import { signUp } from '../../services/user'
 import GlobalContext from '../../global/GlobalContext'
+
+// MATERIAL UI
+import IconButton from '@material-ui/core/IconButton';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const SignUpForm = () => {
   const history = useHistory()
   const [form, onChange, clear] = useForm({ name: '', email: '', cpf: '', password: '' })
   const [isLoading, setIsLoading] = useState(false)
   const { setters } = useContext(GlobalContext)
+  const [showPassword, setShowPassword] = useState(false)
+  const [showPasswordConf, setShowPasswordConf] = useState(false)
 
   const onSubmitForm = (event) => {
     event.preventDefault()
@@ -63,30 +75,57 @@ const SignUpForm = () => {
             inputProps={{ pattern: "[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}" }}
             helperText="Formato 000.111.222-33"
           />
-          <TextField
+         
+          <FormControl variant="outlined" style={{width: '100%', marginTop:10, marginBottom:8}}>
+          <InputLabel htmlFor="passwordField" style={{margin: -7, marginLeft: 1}}>Senha</InputLabel>
+          <OutlinedInput
+            id="passwordField"
+            type={showPassword ? 'text' : 'password'}
             value={form.password}
             name={'password'}
             onChange={onChange}
-            label={'Senha'}
-            variant={'outlined'}
-            type={'password'}
-            fullWidth
-            required
             margin={'dense'}
-            helperText="Deve ter no mínimo 6 dígitos"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={e => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={50}
           />
-          <TextField
+          <FormHelperText id="outlined-weight-helper-text">Deve ter no mínimo 6 dígitos</FormHelperText>
+          </FormControl>
+
+          <FormControl variant="outlined" style={{width: '100%', marginTop:10, marginBottom:8}}>
+          <InputLabel htmlFor="passwordField2" style={{margin: -7, marginLeft: 1}}>Confirmação de senha</InputLabel>
+          <OutlinedInput
+            id="passwordField2"
+            type={showPasswordConf ? 'text' : 'password'}
             value={form.passwordConfirmation}
             name={'passwordConfirmation'}
             onChange={onChange}
-            label={'Confirmar a senha'}
-            variant={'outlined'}
-            type={'password'}
-            fullWidth
-            required
             margin={'dense'}
-            helperText="A confirmação deve ser igual à senha"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={e => setShowPasswordConf(!showPasswordConf)}
+                  edge="end"
+                >
+                  {showPasswordConf ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={170}
           />
+          <FormHelperText id="outlined-weight-helper-text">Deve ter no mínimo 6 dígitos</FormHelperText>
+          </FormControl>
+        
         </InputsContainer>
         <Button
           color={'primary'}
