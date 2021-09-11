09280ef5a5210f3c2956856d814c08f420006142
Gustavo Guimaraes <Gustavo.carvg@gmail.com>
2021-09-11 18:25:13 -0300

    Ajustado o pedido para que só seja possível fazer de um restaurante

--- src/components/CardProducts/CardModal.js
+++ src/components/CardProducts/CardModal.js
@@ -1,65 +1,83 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import UIModal from "../Modal/UIModal";
-import {Button, MenuItem, TextField} from "@material-ui/core";
+import { Button, MenuItem, TextField } from "@material-ui/core";
 import GlobalContext from "../../global/GlobalContext";
 
-const quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const quantities = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const CardModal = (props) => {
-    const {states, setters, requests} = useContext(GlobalContext);
-    const [quantity, setQuantity] = useState(0);
-    const handleChange = (event) => {
-        setQuantity(event.target.value);
-    };
+  const { states, setters, requests } = useContext(GlobalContext);
+  const [quantity, setQuantity] = useState(0);
+  const handleChange = (event) => {
+    setQuantity(event.target.value);
+  };
 
-    const addItems = () => {
-        const productInArray = states.addedProducts.find((product) => {
-            return props.product.id === product.food.id;
-        });
+  const setRestaurantAndProducts = () => {
+    if (states.infoRestaurant === 0) {
+      setters.setInfoRestarant(props.infoRestaurant);
+      addItems();
+    } else if (states.infoRestaurant === props.infoRestaurant) {
+      addItems();
+    } else {
+      props.setShowModal(false);
+      alert("Você só pode adicionar pedidos de um restaurante");
+    }
+  };
 
-        if (productInArray) {
-            const newProduct = states.addedProducts.map((product) => {
-                if (props.product.id === product.food.id) {
-                    return {
-                        ...product,
-                        quantity: quantity
-                    };
-                }
-                return product;
-            });
+  const addItems = () => {
+    const productInArray = states.addedProducts.find((product) => {
+      return props.product.id === product.food.id;
+    });
 
-            setters.setAddedProducts(newProduct);
-        } else {
-            setters.setAddedProducts([
-                ...states.addedProducts,
-                {
-                    food: props.product,
-                    quantity: quantity,
-                    restaurantId: props.infoRestaurant
-                }
-            ]);
+    if (productInArray) {
+      const newProduct = states.addedProducts.map((product) => {
+        if (props.product.id === product.food.id) {
+          return {
+            ...product,
+            quantity: quantity,
+          };
         }
-        props.setShowModal(false);
-    };
+        return product;
+      });
 
-    return (
-        <UIModal isOpen={Boolean(props.itemId)}>
-            <h4>Selecione a quantidade desejada</h4>
+      setters.setAddedProducts(newProduct);
+    } else {
+      setters.setAddedProducts([
+        ...states.addedProducts,
+        {
+          food: props.product,
+          quantity: quantity,
+          restaurantId: props.infoRestaurant,
+        },
+      ]);
+    }
+    props.setShowModal(false);
+  };
 
-            <TextField select variant={"outlined"} fullWidth value={quantity ? quantity : ""} onChange={handleChange}>
-                {quantities.map((number) => {
-                    return (
-                        <MenuItem key={number} value={number}>
-                            {number}
-                        </MenuItem>
-                    );
-                })}
-            </TextField>
+  return (
+    <UIModal isOpen={Boolean(props.itemId)}>
+      <h4>Selecione a quantidade desejada</h4>
 
-            <Button variant={"contained"} onClick={addItems}>
-                Adicionar ao Carrinho
-            </Button>
-        </UIModal>
-    );
+      <TextField
+        select
+        variant={"outlined"}
+        fullWidth
+        value={quantity ? quantity : ""}
+        onChange={handleChange}
+      >
+        {quantities.map((number) => {
+          return (
+            <MenuItem key={number} value={number}>
+              {number}
+            </MenuItem>
+          );
+        })}
+      </TextField>
+
+      <Button variant={"contained"} onClick={setRestaurantAndProducts}>
+        Adicionar ao Carrinho
+      </Button>
+    </UIModal>
+  );
 };
 
 export default CardModal;
--- src/global/GlobalStateContext.js
+++ src/global/GlobalStateContext.js
@@ -1,91 +1,111 @@
 import React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import GlobalContext from "./GlobalContext";
 import axios from "axios";
-import {BASE_URL} from "../constants/urls";
+import { BASE_URL } from "../constants/urls";
 
 const GlobalStateContext = (props) => {
-    const [maximumDeliveryTime, setMaximumDeliveryTime] = useState(0);
-    const [waitingRestaurantName, setWaitingRestaurantName] = useState("");
-    const [subtotalToWait, setSubtotalToAlert] = useState(0.0);
-    const [addedProducts, setAddedProducts] = useState([]);
-    const [infoRestaurant, setInfoRestarant] = useState({})
-    const [infoUserAddress, setInfoUserAddress] = useState({})
+  const [maximumDeliveryTime, setMaximumDeliveryTime] = useState(0);
+  const [waitingRestaurantName, setWaitingRestaurantName] = useState("");
+  const [subtotalToWait, setSubtotalToAlert] = useState(0.0);
+  const [addedProducts, setAddedProducts] = useState([]);
+  const [infoRestaurant, setInfoRestarant] = useState(0);
+  const [infoUserAddress, setInfoUserAddress] = useState({});
 
-    const waitingDelivery = {
-        maximumDeliveryTime,
-        subtotalToWait,
-        waitingRestaurantName
+  const waitingDelivery = {
+    maximumDeliveryTime,
+    subtotalToWait,
+    waitingRestaurantName,
+  };
+
+  const setWaitingDelivery = (
+    maximumDeliveryTime,
+    subtotalToWait,
+    waitingRestaurantName
+  ) => {
+    setMaximumDeliveryTime(maximumDeliveryTime);
+    setWaitingRestaurantName(waitingRestaurantName);
+    setSubtotalToAlert(subtotalToWait);
+  };
+
+  const getActiveOrder = () => {
+    const token = localStorage.getItem("token");
+    const headers = {
+      headers: {
+        auth: token,
+      },
     };
+    axios
+      .get(`${BASE_URL}/fourFoodA/active-order`, headers)
+      .then((response) => {
+        const order = response.data.order;
 
-    const setWaitingDelivery = (maximumDeliveryTime, subtotalToWait, waitingRestaurantName) => {
-        setMaximumDeliveryTime(maximumDeliveryTime);
-        setWaitingRestaurantName(waitingRestaurantName);
-        setSubtotalToAlert(subtotalToWait);
-    };
-
-    const getActiveOrder = () => {
-        const token = localStorage.getItem("token");
-        const headers = {
-            headers: {
-                auth: token
-            }
-        };
-        axios
-            .get(`${BASE_URL}/fourFoodA/active-order`, headers)
-            .then((response) => {
-                const order = response.data.order;
-
-                if (order && order !== null) {
-                    const dt = Number(order.expiresAt) - Number(order.createdAt);
-                    setWaitingDelivery(dt, order.totalPrice, order.restaurantName);
-                }
-            })
-            .catch((err) => {
-                if (err.response) {
-                    alert(err.response.data.message);
-                } else {
-                    alert("Erro ao buscar o alerta de pedido");
-                }
-            });
-    };
-
-    const [userProfile, setUserProfile] = useState({});
-
-    useEffect(() => {
-        if (maximumDeliveryTime > 0) {
-            setTimeout(() => {
-                setMaximumDeliveryTime(0);
-            }, maximumDeliveryTime / 100);
+        if (order && order !== null) {
+          const dt = Number(order.expiresAt) - Number(order.createdAt);
+          setWaitingDelivery(dt, order.totalPrice, order.restaurantName);
         }
-    }, [maximumDeliveryTime]);
+      })
+      .catch((err) => {
+        if (err.response) {
+          alert(err.response.data.message);
+        } else {
+          alert("Erro ao buscar o alerta de pedido");
+        }
+      });
+  };
 
-    const getProfile = () => {
-        const token = localStorage.getItem("token");
-        const headers = {
-            headers: {
-                auth: token
-            }
-        };
-        axios
-            .get(`${BASE_URL}/fourFoodA/profile`, headers)
-            .then((res) => {
-                setUserProfile(res.data.user);
-            })
-            .catch((err) => {
-                if (err.response) {
-                    alert(err.response.data.message);
-                } else {
-                    alert("Erro ao buscar perfil");
-                }
-            });
+  const [userProfile, setUserProfile] = useState({});
+
+  useEffect(() => {
+    if (maximumDeliveryTime > 0) {
+      setTimeout(() => {
+        setMaximumDeliveryTime(0);
+      }, maximumDeliveryTime / 100);
+    }
+  }, [maximumDeliveryTime]);
+
+  const getProfile = () => {
+    const token = localStorage.getItem("token");
+    const headers = {
+      headers: {
+        auth: token,
+      },
     };
+    axios
+      .get(`${BASE_URL}/fourFoodA/profile`, headers)
+      .then((res) => {
+        setUserProfile(res.data.user);
+      })
+      .catch((err) => {
+        if (err.response) {
+          alert(err.response.data.message);
+        } else {
+          alert("Erro ao buscar perfil");
+        }
+      });
+  };
 
-    const states = {waitingDelivery, userProfile, addedProducts, infoRestaurant, infoUserAddress};
-    const setters = {setWaitingDelivery, setUserProfile, setAddedProducts, setInfoRestarant, setInfoUserAddress};
-    const requests = {getProfile, getActiveOrder};
+  const states = {
+    waitingDelivery,
+    userProfile,
+    addedProducts,
+    infoRestaurant,
+    infoUserAddress,
+  };
+  const setters = {
+    setWaitingDelivery,
+    setUserProfile,
+    setAddedProducts,
+    setInfoRestarant,
+    setInfoUserAddress,
+  };
+  const requests = { getProfile, getActiveOrder };
 
-    return <GlobalContext.Provider value={{states, setters, requests}}>{props.children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider value={{ states, setters, requests }}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
 };
 
 export default GlobalStateContext;
