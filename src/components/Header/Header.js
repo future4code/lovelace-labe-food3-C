f8ae34316f2e21bbab11fdadbad5c92356967629
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-12 14:20:54 -0300

    header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -58,6 +58,7 @@ const Header = ({pageTitle="", setPageTitle}) => {
                 }
                 </div>
                 <p>{pageTitle}</p>
+                <div />
             </StyledHeader>
         </>
     )
